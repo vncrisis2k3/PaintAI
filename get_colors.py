c56c709b53502232b32304d9036814517246dcde
Vncrisis08042003 <datctd.21ce@vku.udn.vn>
2026-05-29 11:03:57 +0700

    a

--- get_colors.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('mauson.txt') as f:
-    data = json.load(f)
-    
-colors = data['data']['colors']
-print(f"Total colors: {len(colors)}\n")
-for c in colors[:40]:
-    print(f"{c['paint_code']}: {c['hex_code']}")
